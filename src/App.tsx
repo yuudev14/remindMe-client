5ce80c1760c9b4ed630532afefa9f9e5b2f0ba33
yutakaki <takaki.yu.dev@gmail.com>
2022-01-25 23:23:43 +0900

    install-dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route, HashRouter as Router, Routes } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
   return (
     <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />}/>
+          <Route path="/register" element={<Register />}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
