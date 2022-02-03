9431efe83fda350c4a6f627a4c5355e36e5e9734
yutakaki <takaki.yu.dev@gmail.com>
2022-02-03 11:30:30 +0900

    finish layout and design of detailsSection

---
+++ src/components/Home/TodosSection.tsx
@@ -0,0 +1,108 @@
+import React from 'react';
+
+const TodosSection = () => {
+  return (
+    <section className='todos-section'>
+      <div className='options'>
+        
+      </div>
+      <ul className="todos-dates">
+        <li>
+          <h3>April 2</h3>
+          <ul className="todos-lists">
+
+            <li className="todo">
+              <div className="date">
+                <i></i>
+                <p>12:00 pm</p>
+              </div>
+              <div className="todo-info">
+                <h3>Conference Meeting</h3>
+              </div>
+            </li>
+
+            <li className="todo">
+              <div className="date">
+                <i></i>
+                <p>12:00 pm</p>
+              </div>
+              <div className="todo-info">
+                <h3>Conference Meeting</h3>
+              </div>
+            </li>
+
+            <li className="todo">
+              <div className="date">
+                <i></i>
+                <p>12:00 pm</p>
+              </div>
+              <div className="todo-info">
+                <h3>Conference Meeting</h3>
+              </div>
+            </li>
+
+            <li className="todo">
+              <div className="date">
+                <i></i>
+                <p>12:00 pm</p>
+              </div>
+              <div className="todo-info">
+                <h3>Conference Meeting</h3>
+              </div>
+            </li>
+
+          </ul>
+        </li>
+
+        <li>
+          <h3>April 2</h3>
+          <ul className="todos-lists">
+
+            <li className="todo important">
+              <div className="date">
+                <i></i>
+                <p>12:00 pm</p>
+              </div>
+              <div className="todo-info">
+                <h3>Conference Meeting</h3>
+              </div>
+            </li>
+
+            <li className="todo">
+              <div className="date">
+                <i></i>
+                <p>12:00 pm</p>
+              </div>
+              <div className="todo-info">
+                <h3>Conference Meeting</h3>
+              </div>
+            </li>
+
+            <li className="todo important">
+              <div className="date">
+                <i></i>
+                <p>12:00 pm</p>
+              </div>
+              <div className="todo-info">
+                <h3>Conference Meeting</h3>
+              </div>
+            </li>
+
+            <li className="todo important">
+              <div className="date">
+                <i></i>
+                <p>12:00 pm</p>
+              </div>
+              <div className="todo-info">
+                <h3>Conference Meeting</h3>
+              </div>
+            </li>
+
+          </ul>
+        </li>
+      </ul>
+    </section>
+  );
+};
+
+export default TodosSection;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,109 +1,42 @@
 import NavSection from "../components/Home/NavSection";
+import TodosSection from "../components/Home/TodosSection";
 import "../styles/home.scss";
 
 const Home = () => {
   return (
     <main>
       <NavSection />
-      <section className='todos-section'>
-        <ul className="todos-dates">
-          <li>
-            <h3>April 2</h3>
-            <ul className="todos-lists">
-
-              <li className="todo">
-                <div className="date">
-                  <i></i>
-                  <p>12:00 pm</p>
-                </div>
-                <div className="todo-info">
-                  <h3>Conference Meeting</h3>
-                </div>
-              </li>
-
-              <li className="todo">
-                <div className="date">
-                  <i></i>
-                  <p>12:00 pm</p>
-                </div>
-                <div className="todo-info">
-                  <h3>Conference Meeting</h3>
-                </div>
-              </li>
-
-              <li className="todo">
-                <div className="date">
-                  <i></i>
-                  <p>12:00 pm</p>
-                </div>
-                <div className="todo-info">
-                  <h3>Conference Meeting</h3>
-                </div>
-              </li>
-
-              <li className="todo">
-                <div className="date">
-                  <i></i>
-                  <p>12:00 pm</p>
-                </div>
-                <div className="todo-info">
-                  <h3>Conference Meeting</h3>
-                </div>
-              </li>
-
-            </ul>
-          </li>
-
-          <li>
-            <h3>April 2</h3>
-            <ul className="todos-lists">
-
-              <li className="todo">
-                <div className="date">
-                  <i></i>
-                  <p>12:00 pm</p>
-                </div>
-                <div className="todo-info">
-                  <h3>Conference Meeting</h3>
-                </div>
-              </li>
-
-              <li className="todo">
-                <div className="date">
-                  <i></i>
-                  <p>12:00 pm</p>
-                </div>
-                <div className="todo-info">
-                  <h3>Conference Meeting</h3>
-                </div>
-              </li>
-
-              <li className="todo">
-                <div className="date">
-                  <i></i>
-                  <p>12:00 pm</p>
-                </div>
-                <div className="todo-info">
-                  <h3>Conference Meeting</h3>
-                </div>
-              </li>
-
-              <li className="todo">
-                <div className="date">
-                  <i></i>
-                  <p>12:00 pm</p>
-                </div>
-                <div className="todo-info">
-                  <h3>Conference Meeting</h3>
-                </div>
-              </li>
-
-            </ul>
-          </li>
-        </ul>
-      </section>
+      <TodosSection />
       <section className='calendar-details-section'>
         <section className='details-section'>
+          <div className="details">
+            <p className="fade">Saturday April 22</p>
+            <i></i>
+          </div>
+          <div className="details">
+            <h1>Todo Title</h1>
+            <i></i>
+          </div>
+          <div className="details-inline">
+            <span className="tag">Work</span>
+            <span className="tag">Work</span>
+            <span className="tag">Work</span>
+          </div>
+          <div className="details-inline">
+            <div>
+              <p className="fade">Duration</p>
+              <h2>7hrs</h2>
+            </div>
+            <div>
+              <p className="fade">Time start</p>
+              <h2>13:00 pm</h2>
+            </div>
+          </div>
+          <article className="">
+            <p>Ipsum magna adipisicing dolore minim dolor occaecat veniam in dolore. Commodo cupidatat aliquip consequat incididunt. Cillum esse culpa fugiat veniam voluptate eu id eu. Tempor aliquip nostrud reprehenderit elit ullamco id incididunt aute veniam. Anim culpa occaecat eiusmod magna incididunt ad ipsum ex qui. Esse sunt incididunt consectetur commodo.</p>
+            <br />
+            <p>Ipsum magna adipisicing dolore minim dolor occaecat veniam in dolore. Commodo cupidatat aliquip consequat incididunt. Cillum esse culpa fugiat veniam voluptate eu id eu. Tempor aliquip nostrud reprehenderit elit ullamco id incididunt aute veniam. Anim culpa occaecat eiusmod magna incididunt ad ipsum ex qui. Esse sunt incididunt consectetur commodo.</p>
+          </article>
         </section>
         <section className='calendar-section'>
         </section>
